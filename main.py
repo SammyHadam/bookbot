51f3268feaccd04257dfbaf5ce6c1aa7344bc618
Sammy <122151547+SammyHadam@users.noreply.github.com>
2024-12-19 10:24:51 -0600

    completed character count function on MacBook

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = word_count(text)
+    char_count = character_count(text)
     print(text)
     print(f"{count} words found in the document")
+    print(f"{char_count}")
 
 def get_book_text(path):
     with open(path) as f:
-        return f.read()
+    return f.read()
 
 def word_count(text):
     counter = 0
@@ -17,8 +19,14 @@ def word_count(text):
     return counter
 
 def character_count(text):
-    words = text.split.lower()
-    characters = words.split()
-
+    letters = {}
+    characters = text.lower()
+    for char in characters:
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] = 1
+    return letters
 
 main()
+
