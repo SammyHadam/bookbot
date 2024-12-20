76be123d7694579d8a99d5293c059321cdff6391
SammyHadam <sammyhadams@gmail.com>
2024-12-20 13:30:16 -0600

    updated character count function to create a sorted list of characters in ascending order

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = word_count(text)
-    char_count = character_count(text)
-    print(text)
-    print(f"{count} words found in the document")
-    print(f"{char_count}")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count} words found in the document\n")
+    character_count(text)
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,11 +22,22 @@ def character_count(text):
     letters = {}
     characters = text.lower()
     for char in characters:
-        if char in letters:
-            letters[char] += 1
-        else:
-            letters[char] = 1
-    return letters
+        if char.isalpha():
+            if char in letters:
+                letters[char] += 1
+            else:
+                letters[char] = 1
+
+    list_of_letters = [{"char": char, "num": count} for char, count in letters.items()]
+    def sort_on(letter):
+        return letter["num"]
+    
+    list_of_letters.sort(reverse=True, key=sort_on)
+    for alphs in list_of_letters:
+        char = alphs["char"]
+        count = alphs["num"]
+        print(f"The '{char}' character was found {count} times")
+    
 
 main()
 
